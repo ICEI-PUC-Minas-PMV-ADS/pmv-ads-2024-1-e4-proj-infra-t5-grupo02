f689e95f2bb2c17832cef84cc4602b79d6d04c1f
thaisoliveira1356480 <115051179+thaisoliveira1356480@users.noreply.github.com>
2024-03-22 20:00:07 -0300

    criação do projeto login

---
+++ src/EasyRent/APILogin/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using APILogin.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APILogin.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Senha { get; set; }
+        [Required]
+        public Perfil Perfil { get; set; }
+
+
+    }
+
+}
