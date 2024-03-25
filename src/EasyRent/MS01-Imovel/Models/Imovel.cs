fa3ab6b87ed88036966e27531aa3a9e1e7f605f8
leonardobuckalves <leonardobuckalves@gmail.com>
2024-03-25 09:42:37 -0300

    MS01 v0.1

---
+++ src/EasyRent/MS01-Imovel/Models/Imovel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MS01_Imovel.Models
+{
+    public class Imovel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Endereco { get; set; } = null!;
+        public double Valor { get; set; }
+        public string Descricao { get; set; } = null!;
+        public Status Status { get; set; }
+    }
+}
