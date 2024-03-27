1734017712438d91bf2d02ebc7d010dd815672e2
leonardobuckalves <leonardobuckalves@gmail.com>
2024-03-27 17:27:59 -0300

    Imovel model atualizado

--- src/EasyRent/MS01-Imovel/Models/Imovel.cs
+++ src/EasyRent/MS01-Imovel/Models/Imovel.cs
@@ -9,8 +9,19 @@ namespace MS01_Imovel.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string Endereco { get; set; } = null!;
-        public double Valor { get; set; }
-        public string Descricao { get; set; } = null!;
+        public string Complemento { get; set; } = null!;
+        public string Cidade { get; set; } = null!;
+        public string Estado { get; set; } = null!;
+        public string CEP { get; set; } = null!;
+        public int QtdQuartos { get; set; }
+        public int QtdBanheiros { get; set; }
+        public int QtdVagasGaragem { get; set; }
+        public float AreaTotal { get; set; }
+        public double ValorAluguel { get; set; }
+        public double ValorCondominio { get; set; }
+        public string DescricaoDetalhada { get; set; } = null!;
         public Status Status { get; set; }
+        public TipoImovel TipoImovel { get; set; }
+        public byte[] Imagem { get; set; } = new byte[] { };
     }
 }
