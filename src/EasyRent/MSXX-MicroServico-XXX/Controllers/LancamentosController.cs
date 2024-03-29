dc34f1ed952cba5e3b97e2f61e4a233f6669e58b
Leonardo Lima <leodmelo@hotmail.com>
2024-03-29 01:32:26 -0300

    Create model e controller Lançamentos

---
+++ src/EasyRent/MSXX-MicroServico-XXX/Controllers/LancamentosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MSXX_MicroServico_XXX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LancamentosController : ControllerBase
+    {
+       
+    }
+}
---
+++ src/EasyRent/MSXX-MicroServico-XXX/Models/Lancamento.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MSXX_MicroServico_XXX.Models
+{
+    [Table("Lancamentos")]
+    public class Lancamento
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Lançamento")]
+        public Lancamento Tipo { get; set; }
+
+
+        public Forma Forma { get; set; }
+
+        [Display(Name = "Classificação")]
+        public Classificacao Classificacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data!")]
+        [Display(Name = "Data de Vencimento")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o status!")]
+        public StatusTransacao Status { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [Required(ErrorMessage = "É necessário informar o valor!")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Obrigatório informar a descrição!")]
+        public string Descricao { get; set; }
+
+    }
+
+    public enum Tipo
+    {
+        Receita,
+        Despesa
+    }
+
+    public enum Forma
+    {
+        Dinheiro,
+        Cartão,
+        Pix
+    }
+
+    public enum Classificacao
+    {
+        Aluguel,
+        IPTU,
+        TCR
+    }
+
+    public enum StatusTransacao
+    {
+        Pendente,
+        Pago,
+        APagar
+    }
+
+
+}
