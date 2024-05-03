0950f89f873ab1f97364eedb2e2111a532800a83
Leonardo Lima <leodmelo@hotmail.com>
2024-05-02 23:29:00 -0300

    Login (front)

---
+++ src/FrontEnd/Cadastro.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.getElementById('usuariosTable')) {
+        fetchUsuarios();
+    }
+    
+    const form = document.getElementById('frm1');
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+  
+            const formData = {
+              nome: document.getElementById('nome').value,
+              email: document.getElementById('email').value,
+              senha: document.getElementById('senha').value,
+              perfil: document.getElementById('tipo').value
+            };
+  
+            fetch('https://localhost:7194/api/Usuarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(data => {
+                        throw new Error(data.message || "Erro desconhecido");
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Usuário criado:', data);
+                alert('Usuário adicionado com sucesso!');
+                form.reset();
+                if (document.getElementById('Usuarios')) {
+                    fetchUsuarios();
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao criar Usuário:', error);
+                alert('Falha ao adicionar Usuário: ' + error.message);
+            });
+        });
+    }
+  });
