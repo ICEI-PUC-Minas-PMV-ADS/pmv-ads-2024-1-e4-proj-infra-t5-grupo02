acbff37535ef97276a94962bd506c3377793ef74
leonardobuckalves <leonardobuckalves@gmail.com>
2024-06-01 21:44:20 -0300

    restrição de rotas

--- src/AppEasyRent/services/urls.js
+++ src/AppEasyRent/services/urls.js
@@ -1,4 +1,4 @@
 /* export const IMOVEIS_URL = "https://82b9-150-165-11-137.ngrok-free.app"     // :7030 */
-export const FINANCEIRO_URL = "https://9bf5-2804-14c-9b83-99ab-8102-ae79-3435-c938.ngrok-free.app"  // :7157
-export const LOGIN_URL = "https://3057-2804-14c-9b83-99ab-8102-ae79-3435-c938.ngrok-free.app"  // :7194
-export const INQUILINOS_URL = "https://07c7-2804-14c-9b83-99ab-8102-ae79-3435-c938.ngrok-free.app"  // :7294
+export const FINANCEIRO_URL = "https://6c7d-2804-1e68-8003-fc7a-84df-1c47-a90b-1c3b.ngrok-free.app"  // :7157
+export const LOGIN_URL = "https://ab84-2804-1e68-8003-fc7a-84df-1c47-a90b-1c3b.ngrok-free.app"  // :7194
+export const INQUILINOS_URL = "https://2a5d-2804-1e68-8003-fc7a-84df-1c47-a90b-1c3b.ngrok-free.app"  // :7294
