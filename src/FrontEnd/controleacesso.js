33aae3fdc5626f75208a64f1d27dfc1e6436ddfc
Leonardo Lima <leodmelo@hotmail.com>
2024-06-16 21:54:04 -0300

    updates

--- src/FrontEnd/controleacesso.js
+++ src/FrontEnd/controleacesso.js
@@ -15,6 +15,7 @@ function adjustNavLinks() {
         document.querySelector('a[href="Imoveis.html"]').parentNode.style.display = '';
         document.querySelector('a[href="Financeiro.html"]').parentNode.style.display = '';
         document.querySelector('a[href="Inquilino.html"]').parentNode.style.display = '';
+        document.querySelector('a[href="Dashboard.html"]').parentNode.style.display = '';
     } else if (userProfile === 'Inquilino') {
         document.querySelector('a[href="User.html"]').parentNode.style.display = '';
         document.querySelector('a[href="Financeiro.html"]').parentNode.style.display = '';
@@ -50,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const userProfile = localStorage.getItem('profile');
     const allowedPages = {
         'Administrador': ['User.html', 'Imoveis.html', 'Financeiro.html', 'Inquilino.html', 'CadastroUsuario.html', 'Usuarios.html', 'CadastroUsuario.html'],
-        'Locador': ['User.html', 'Imoveis.html', 'Financeiro.html', 'LancamentoFinan.html', 'EditarLancamento.html', 'Inquilino.html', 'CadastroInquilino.html', 'EditarInquilino.html', 'CadastroImob.html'],
+        'Locador': ['User.html', 'Dashboard.html', 'Imoveis.html', 'Financeiro.html', 'LancamentoFinan.html', 'EditarLancamento.html', 'Inquilino.html', 'CadastroInquilino.html', 'EditarInquilino.html', 'CadastroImob.html'],
         'Inquilino': ['User.html', 'Financeiro.html']
     };
 
